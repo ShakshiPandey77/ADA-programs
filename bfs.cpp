b3812445cf00c93b78206738a70e4c662bed4f38
ShakshiPandey77 <49754283+ShakshiPandey77@users.noreply.github.com>
2019-09-12 20:46:48 +0530

    Update bfs.cpp

--- bfs.cpp
+++ bfs.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/* #include<iostream>
 using namespace std;
 int a[20][20], q[20], visited[20], n, i, j, f = 0, r = -1;
 
@@ -43,4 +43,50 @@ cin>>v;
  }
  }
 return 0;
+} */
+
+//
+#include <iostream>
+#include <queue> // STL queue
+using namespace std;
+
+queue<int> q;
+
+void BFS(int k,int **adj,bool *visited, int n){
+  cout<<k+1<<" ";
+  visited[k] = true;
+  q.push(k);
+  while(!q.empty()){
+    int curr = q.front();
+    q.pop();
+    for(int i =0; i<n;i++){
+      if(adj[curr][i] && !visited[i]){
+        q.push(i);
+        visited[i] = true;
+        cout<<i+1<<" ";
+      }
+    }
+  }
 }
+
+int main(){
+  int **adj, k, n;
+  bool *visited;
+  cout<<"Enter the number of nodes :\n";
+  cin>>n;
+  visited = new bool[n];
+  adj = new int*[n];
+  for(int i=0; i<n;i++)
+    adj[i] = new int[n];
+  cout<<"Enter Adjacency Matrix :\n";
+  for(int i=0; i<n;i++)
+    for(int j =0; j<n;j++)
+      cin>>adj[i][j];
+  cout<<"Enter the source vertex :";
+  cin>>k;
+  BFS(k-1,adj,visited,n);
+  return 0;
+}
+
+
+
