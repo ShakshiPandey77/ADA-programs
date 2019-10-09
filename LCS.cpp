be435536fbca70447698914cd7f9db7392a44a89
ShakshiPandey77 <49754283+ShakshiPandey77@users.noreply.github.com>
2019-10-09 19:59:29 +0530

    Update LCS.cpp

--- LCS.cpp
+++ LCS.cpp
@@ -1,57 +1,57 @@
-#include<iostream>
-#include<cstring>
-using namespace std;
-int max(int a,int b);
-void LCS(string X,string Y,int m,int n)
-{
-int L[m+1][n+1];
-    for(int i=0;i<=m;i++)
-        {
-            for(int j=0;j<=n;j++)
-                {
-                    if(i==0 || j==0)
-                        L[i][j]=0;
-                    else if(X[i-1]==Y[j-1])
-                        L[i][j]=L[i-1][j-1]+1;
-                    else
-                        L[i][j]=max(L[i-1][j],L[i][j-1]);
-                }
-        }
-int index=L[m][n];
-char lcs[index+1];
-lcs[index]='\0';
-int i=m,j=n;
-    while(i>0&&j>0)
-        {
-            if(X[i-1]==Y[j-1])
-                {
-                    lcs[index-1]=X[i-1];
-                    i--;
-                    j--;
-                    index--;
-                }
-            else if(L[i-1][j]>L[i][j-1])
-                i--;
-            else
-                j--;
-        }
-        cout<<"The LCS is "<<lcs<<endl;
-        }
-int max(int a,int b)
-{
-return (a>b?a:b);
-}
+#include<iostream> 
+using namespace std; 
 
-int main()
-{
-string X,Y;
-cout<<"enter the string 1:";
-cin>>X;
-cout<<"enter the string 2:";
-cin>>Y;
-int m=X.length();
-int n=Y.length();
-LCS(X,Y,m,n);
-return 0;
-}
-
+void lcs( char *X, char *Y, int m, int n ) 
+  {
+   int L[m+1][n+1]; 
+   for (int i=0; i<=m; i++) 
+   { 
+     for (int j=0; j<=n; j++) 
+     { 
+       if (i == 0 || j == 0) 
+         L[i][j] = 0; 
+       else if (X[i-1] == Y[j-1]) 
+         L[i][j] = L[i-1][j-1] + 1; 
+       else
+         L[i][j] = max(L[i-1][j], L[i][j-1]); 
+     } 
+   } 
+   cout<<"length of LCS:"<<L[m][n]<<endl;
+   //printing lcs
+   int index = L[m][n]; 
+   char lcs[index+1]; 
+   lcs[index] = '\0'; // Set the terminating character 
+   int i = m, j = n; 
+   while (i > 0 && j > 0) 
+   { 
+      if (X[i-1] == Y[j-1]) 
+      { 
+          lcs[index-1] = X[i-1];  
+          i--; 
+          j--; 
+          index--;     
+      } 
+  
+      else if (L[i-1][j] > L[i][j-1]) 
+         i--; 
+      else
+         j--; 
+   } 
+  cout << "LCS of " << X << " and " << Y << " is " << lcs<<endl; 
+} 
+  
+int main() 
+{ 
+  // char X[] = "AGGTAB"; 
+  // char Y[] = "GXTXAYB";
+  char X[20],Y[20]; 
+  cout<<"enter the 1st string:";
+  cin>>X;
+  cout<<"enter the 2nd string:";
+  cin>>Y;
+  
+  int m = strlen(X); 
+  int n = strlen(Y); 
+  lcs(X, Y, m, n); 
+  return 0; 
+} 
